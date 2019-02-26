af7a855f53e8a1afc4a84b1a4360c28e73e2fbbb
Naomi Katz <naykatz@gmail.com>
2019-02-26 02:45:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+attr_accessor :name, :roster
+
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end   
+    
+    
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade].push(student_name)
+    else 
+      @roster[grade] = [student_name]
+    end
+  end
+
+
+  def grade(grade)
+    @roster[grade]
+  end
+ 
+end
+
+
+
+
+school = School.new("Bayside High School ")
